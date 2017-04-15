e7d0b3b14ce84edb3211febb10c99d98689cecc7
Kouma Motooka <kouma1990@gmail.com>
2017-04-15 05:41:54 +0000

    modify: chartjs version 2.1.4->2.1.6

--- resources/views/chart/home_chart.blade.php
+++ resources/views/chart/home_chart.blade.php
@@ -1,4 +1,4 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.min.js"></script>
 <script>
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
