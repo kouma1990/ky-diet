ffb984cab9642b44e8312e09d558907487c868f4
Kouma Motooka <kouma1990@gmail.com>
2017-04-30 02:27:14 +0000

    add: favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'KY') }}</title>
+    
+    <link rel="icon" href="/favicon.ico" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
